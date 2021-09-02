2066430bfbce669aed211f91420ef9a375c8ef54
devmicaelyg <gusmaomicaely@gmail.com>
2021-09-01 21:46:30 -0300

    log project

---
+++ SerilogWebApp/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using Serilog; 
+
+namespace SerilogWebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json").Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config).CreateLogger();
+
+            try
+            {
+                Log.Information("Api inicializando..");
+                CreateHostBuilder(args).Build().Run();
+
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "A aplicação falhou a iniciar.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
